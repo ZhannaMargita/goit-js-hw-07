48287f11c90ef21803406c2c50c2580a6388ba4c
Zhanna_Margita <margitazhanna@gmail.com>
2024-11-04 09:45:08 +0100

    editedHomeWork7

--- js/task-6.js
+++ js/task-6.js
@@ -1,47 +1,51 @@
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-const input = document.querySelector('#controls input');
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]');
-const boxesContainer = document.getElementById('boxes');
-
 function createBoxes(amount) {
-  const boxElements = [];
-  let boxSize = 30;
+  destroyBoxes();
+  const boxes = [];
+  let size = 30;
 
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
-    box.style.width = `${boxSize}px`;
-    box.style.height = `${boxSize}px`;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxElements.push(box);
-    boxSize += 10;
+    box.style.margin = '5px';
+    size += 10;
+    boxes.push(box);
   }
 
-  boxesContainer.innerHTML = '';
-  boxesContainer.append(...boxElements);
-}
-
-function handleCreate() {
-  const amount = parseInt(input.value);
-
-  if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
-    input.value = '';
-  }
-}
-
-function handleDestroy() {
-  destroyBoxes();
+  boxesContainer.append(...boxes);
 }
 
 function destroyBoxes() {
   boxesContainer.innerHTML = '';
 }
 
-createButton.addEventListener('click', handleCreate);
-destroyButton.addEventListener('click', handleDestroy);
+function onCreateButtonClick() {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100');
+  }
+}
+
+function onDestroyButtonClick() {
+  destroyBoxes();
+}
+
+createBtn.addEventListener('click', onCreateButtonClick);
+destroyBtn.addEventListener('click', onDestroyButtonClick);
